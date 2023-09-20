781c1c29bfa8dc621142ad8dff9890bca19a4a91
Okwudili-Daniel01~ <makuochukwuliokwudi@gmail.com>
2023-09-20 01:56:45 +0100

    fifty commit

--- practice.ts
+++ practice.ts
@@ -1,82 +1,95 @@
-import http, {IncomingMessage, ServerResponse} from "http"
 
-const port: number = 1111
 
-interface iData{
-    id: number
-    color: string
+import http, { IncomingMessage, ServerResponse } from "http";
+import event from "events";
+const port: number = 9999;
+
+interface iData {
+  id: number;
+  name: string;
+  phone: number;
+  stack: string;
 }
 
-interface iMessage{
-    message: string
-    success: boolean
-    data: null | object | {}[]
+interface iMessage {
+  message: string;
+  success: boolean;
+  data: null | {} | {}[];
 }
 
-const fourth: iData[] = [
-    {
-        id: 9,
-        color: "pink",
-    },
-    {
-        id: 8,
-        color: "gold",
-    },
-    {
-        id: 7,
-        color: "green",
-    },
-    {
-        id: 6,
-        color: "blue",
-    },
-    {
-        id: 5,
-        color: "pink",
-    },
-]
+const set08: iData[] = [
+  {
+    id: 1,
+    name: "joan",
+    phone: 8023474637,
+    stack: "Full-Stack",
+  },
+  {
+    id: 2,
+    name: "Habib",
+    phone: 8023474637,
+    stack: "Full-Stack",
+  },
+  {
+    id: 3,
+    name: "Daniel",
+    phone: 8023474637,
+    stack: "Full-Stack",
+  },
+  {
+    id: 4,
+    name: "SEAN",
+    phone: 8023474637,
+    stack: "Full-Stack",
+  },
+  {
+    id: 5,
+    name: "joan",
+    phone: 8023474637,
+    stack: "Full-Stack",
+  },
+];
 
-const server = http.createServer((req: IncomingMessage, res: ServerResponse) =>{
-    res.setHeader("Content-Type", "application/json")
-
-    const {method, url} = req
+const server = http.createServer(
+  (req: IncomingMessage, res: ServerResponse<IncomingMessage>) => {
+    res.setHeader("Content-Type", "Application/Json");
+    const { method, url } = req;
     let status: number = 404;
 
     let response: iMessage = {
-        message: "failed",
-        success: false,
-        data: null,
-    }
-    const container:any = []
-    req.on("data", (chunk:any) =>{
-        container.push(chunk)
-    }).on("end", () =>{
-        if (url === "/" && method === "GET"){
-            status = 200;
-            response.message = "Success";
-            response.success = true;
-            response.data = fourth;
-            res.write(JSON.stringify({response, status}))
-
-            res.end();
+      message: "failed",
+      success: false,
+      data: null,
+    };
+    const container: any = [];
+    req
+      .on("data", (chunk: any) => {
+        container.push(chunk);
+      })
+      .on("end", () => {
+        if (url === "/" && method === "GET") {
+          status = 200;
+          response.message = "All set08 data gotten";
+          response.success = true;
+          response.data = set08;
+          res.write(JSON.stringify({ response, status }));
+          res.end();
         }
+        if (url === "/" && method === "POST") {
+          status = 201;
+          const body = JSON.parse(container);
+          set08.push(body);
+          response.message = "SUCCESSFULLY added";
+          response.success = true;
+          response.data = set08;
+          res.write(JSON.stringify({ response, status }));
 
-        if (url === "/" && method === "POST"){
-            status = 201;
-            const body = JSON.parse(container)
-            fourth.push(body)
-
-            response.message = "Added";
-            response.success = true;
-            response.data = fourth;
-            res.write(JSON.stringify({response, status}))
-
-            res.end();
+          res.end();
         }
-    })
-    })
+      });
+  }
+);
 
-    server.listen(port, () =>{
-        console.log("Done");
-        
-    })
+server.listen(port, () => {
+  console.log("Server is up and running");
+});
